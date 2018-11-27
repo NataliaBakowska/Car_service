71f539674999c9308550e2878497f9e849b3b945
Łukasz Michalski <lmichalski@9bits.pl>
2018-11-27 16:26:20 +0100

    init project

---
+++ mobile/ProjectMechanicApp/ProjectMechanicApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProjectMechanicApp
+//
+//  Created by Łukasz Michalski on 27.11.2018.
+//  Copyright © 2018 Łukasz Michalski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
